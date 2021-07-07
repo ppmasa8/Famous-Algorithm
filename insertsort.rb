d319bc74712e6f374e332c7bc0af2dce06be81e7
ppmasa8 <kiyokiyopip56@gmail.com>
2021-07-07 14:36:08 +0900

    Update insertsort.rb

--- insertsort.rb
+++ insertsort.rb
@@ -1,18 +1,23 @@
 # array = [1,5,3,7,320]
 class Array
-  def insert_sort
-    (1...self.size).each do |i|
-      temp = self[i]
-      num  = i - 1
-      while num >= 0 && self[num] > temp
-        self[num + 1] = self[num]
-        num -= 1
-      end
-      self[num + 1] = temp
-    end
-    self
-  end
-end
+  require 'benchmark'
 
-array = Array.new(10){ rand(100) }
-puts array.insert_sort
+  result = Benchmark.realtime do
+    def insert_sort
+      (1...self.size).each do |i|
+        temp = self[i]
+        num  = i - 1
+        while num >= 0 && self[num] > temp
+          self[num + 1] = self[num]
+          num -= 1
+        end
+        self[num + 1] = temp
+      end
+      self
+    end
+    array = Array.new(1000){ rand(10000) }
+    p array
+    p array.insert_sort
+  end
+  puts "実行時間 #{result}s"
+end
