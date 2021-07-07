3ae1a1472537b1105a8aa4bfd246d0770e3849db
ppmasa8 <kiyokiyopip56@gmail.com>
2021-07-07 14:30:59 +0900

    Update bubblesort.rb

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,20 +1,25 @@
 # array = [1,4,2,5,30]
 class Array
-  def bubble_sort
-    array_size = self.size - 1
-    while array_size > 1
-      (0..array_size-1).each do |i|
-        if self[i] > self[i+1]
-          array_freeze = self[i+1]
-          self[i+1] = self[i]
-          self[i] = array_freeze
-        end
-      end
-      array_size-=1
-    end
-    self
-  end
-end
+  require 'benchmark'
 
-array = Array.new(10){ rand(100) }
-puts array.bubble_sort
+  result = Benchmark.realtime do
+    def bubble_sort
+      array_size = self.size - 1
+      while array_size > 1
+        (0..array_size-1).each do |i|
+          if self[i] > self[i+1]
+            array_freeze = self[i+1]
+            self[i+1] = self[i]
+            self[i] = array_freeze
+          end
+        end
+        array_size-=1
+      end
+      self
+    end
+    array = Array.new(1000){ rand(10000) }
+    p array
+    p array.bubble_sort
+  end
+  puts "実行時間 #{result}s"
+end
