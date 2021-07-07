2dd22732d19cc7e83d6bb1237ca4cb1232b23ede
ppmasa8 <kiyokiyopip56@gmail.com>
2021-07-07 14:42:56 +0900

    Update selectionsort.rb

--- selectionsort.rb
+++ selectionsort.rb
@@ -1,14 +1,19 @@
 # array = [1,6,3,9,60,47,5]
 class Array
-  def selection_sort
-    new_array = []
-    (self.size).times do
-      new_array << self.min
-      self.delete(self.min)
-    end
-    new_array
-  end
-end
+  require 'benchmark'
 
-array = Array.new(10){ rand(100) }
-puts array.selection_sort
+  result = Benchmark.realtime do
+    def selection_sort
+      new_array = []
+      (self.size).times do
+        new_array << self.min
+        self.delete(self.min)
+      end
+      new_array
+    end
+    array = Array.new(1000){ rand(10000) }
+    p array
+    p array.selection_sort
+  end
+  puts "実行時間 #{result}s"
+end
